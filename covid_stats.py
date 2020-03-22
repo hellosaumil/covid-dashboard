75ec6aac288ceaee9b6102da7f0fe33d45e78594
hellosaumil <6513922+hellosaumil@users.noreply.github.com>
2020-03-22 14:18:02 -0700

    Add: covid_stats.py with class CovidStats

---
+++ covid_stats.py
@@ -0,0 +1,37 @@
+from covid import Covid
+
+
+# countries = covid.list_countries()
+#
+# italy_cases = covid.get_status_by_country_id(115)
+# italy_cases = covid.get_status_by_country_name("italy")
+
+class CovidStats:
+
+    def __init__(self):
+        self.last_covid_data = self.refresh()
+
+    def refresh_data(self):
+        return Covid().get_data()
+
+    def country_stats(self, country_name="italy", refresh=False):
+        country_name = country_name.lower()
+
+        covid = self.refresh_data() if refresh else self.last_covid_data
+
+        if country_name == "us":
+            return covid.get_status_by_country_id(78)
+
+        return covid.get_status_by_country_name(country_name)
+
+
+    def get_stats(self, refresh=False):
+
+        covid = self.refresh_data() if refresh else self.last_covid_data
+
+        active = covid.get_total_active_cases()
+        confirmed = covid.get_total_confirmed_cases()
+        recovered = covid.get_total_recovered()
+        deaths = covid.get_total_deaths()
+
+        print("\nTotal Confirmed: {}\nActive Cases: {}\nRecovered Cases: {}\nDeaths: {}\n".format(confirmed, active, recovered, deaths))
--- user_management.py
+++ user_management.py
@@ -21,3 +21,30 @@ def add_user(uname=None, pwd=None):
 def save_users(users_dict):
     with open(USER_DATABASE_PATH, "w") as write_file:
         json.dump(users_dict, write_file, indent=4)
+
+
+from html.parser import HTMLParser
+from html.entities import name2codepoint
+
+class MyHTMLParser(HTMLParser):
+    def handle_starttag(self, tag, attrs):
+        print("Start tag:", tag)
+        for attr in attrs:
+            print("     attr:", attr)
+    def handle_endtag(self, tag):
+        print("End tag  :", tag)
+    def handle_data(self, data):
+        print("Data     :", data)
+    def handle_comment(self, data):
+        print("Comment  :", data)
+    def handle_entityref(self, name):
+        c = chr(name2codepoint[name])
+        print("Named ent:", c)
+    def handle_charref(self, name):
+        if name.startswith('x'):
+            c = chr(int(name[1:], 16))
+        else:
+            c = chr(int(name))
+        print("Num ent  :", c)
+    def handle_decl(self, data):
+        print("Decl     :", data)
