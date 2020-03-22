6a0e9c03a5a1007d543e930037fd09b3a2838242
hellosaumil <6513922+hellosaumil@users.noreply.github.com>
2020-03-22 15:10:07 -0700

    Add: scheduler support

--- server.py
+++ server.py
@@ -1,6 +1,11 @@
 import os, re, sys, json
 import logging
 from flask import Flask, request, render_template, send_file, make_response, jsonify, redirect, url_for
+from flask_cors import CORS, cross_origin
+
+from covid_stats import CovidStats
+
+from apscheduler.schedulers.background import BackgroundScheduler
 
 session_dict = dict()
 from user_management import load_users, add_user
@@ -11,6 +16,9 @@ logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
 headers = {'Content-Type' : 'application/json'}
 app.secret_key = 'super secret string'  # Change this!
 
@@ -19,16 +27,31 @@ login_manager.init_app(app)
 
 
 @app.route('/', methods=["GET"])
+@cross_origin()
 def dashboardGET():
     return redirect(url_for('dashboard'))
-
-
 
 class User(flask_login.UserMixin):
     pass
 
 def get_users():
     return load_users()
+
+def validate_user(uname=None, pwd=None):
+
+    curr_users = get_users()
+
+    """ Check if user not in userbase """
+    if uname not in curr_users.keys():
+        return False
+
+    if pwd == curr_users[uname]['password']:
+        user = User()
+        user.id = uname
+        flask_login.login_user(user)
+        return True
+
+    return False
 
 @login_manager.user_loader
 def user_loader(email):
@@ -41,7 +64,6 @@ def user_loader(email):
     user = User()
     user.id = email
     return user
-
 
 @login_manager.request_loader
 def request_loader(request):
@@ -62,6 +84,7 @@ def request_loader(request):
     return user
 
 @app.route('/login', methods=['GET'])
+@cross_origin()
 def login():
 
     alert=session_dict.get('alert', None)
@@ -83,29 +106,11 @@ def login():
             </form>
     '''
 
-    if not alert or alert == "":
-        return basic_login_form
-    else:
-        return '''<script type="text/javascript">alert("{}");</script>'''.format(alert) + basic_login_form
-
-
-def validate_user(uname=None, pwd=None):
-
-    curr_users = get_users()
-
-    """ Check if user not in userbase """
-    if uname not in curr_users.keys():
-        return False
-
-    if pwd == curr_users[uname]['password']:
-        user = User()
-        user.id = uname
-        flask_login.login_user(user)
-        return True
-
-    return False
+    if not alert or alert == "": return basic_login_form
+    else: return '''<script type="text/javascript">alert("{}");</script>'''.format(alert) + basic_login_form
 
 @app.route('/login_post', methods=['POST'])
+@cross_origin()
 def login_post():
 
     if validate_user(uname=request.form['username'], pwd=request.form['password']):
@@ -113,24 +118,8 @@ def login_post():
     else:
         return 'Incorrect user/password!'
 
-
-@app.route('/dashboard')
-@flask_login.login_required
-def dashboard():
-
-    alert=session_dict.get('alert', None)
-    if alert: del session_dict['alert']
-
-    dashboard_basic_html = "<h2> {} </h2>".format("Logged in as: " + flask_login.current_user.id)
-
-
-    if not alert or alert == "":
-        return dashboard_basic_html
-
-    else:
-        return '''<script type="text/javascript">alert("{}");</script>'''.format(alert) + dashboard_basic_html
-
 @app.route('/logout')
+@cross_origin()
 def logout():
     flask_login.logout_user()
     # return 'Logged out'
@@ -138,11 +127,12 @@ def logout():
     return redirect(url_for('login'))
 
 @app.route('/signup', methods=["GET"])
+@cross_origin()
 def signup_get():
     return redirect(url_for('login'))
 
-
 @app.route('/signup_post', methods=["POST"])
+@cross_origin()
 def signup_post():
 
     # TODO: Exception handling
@@ -172,12 +162,45 @@ def signup_post():
 
     return redirect(url_for('dashboard'))
 
-
 @login_manager.unauthorized_handler
 def unauthorized_handler():
     # return 'Unauthorized'
     return redirect(url_for('login'))
 
+
+@app.route('/dashboard')
+@cross_origin()
+@flask_login.login_required
+def dashboard():
+
+    alert=session_dict.get('alert', None)
+    if alert: del session_dict['alert']
+
+    dashboard_basic_html = "<h2> {} </h2>".format("Logged in as: " + flask_login.current_user.id)
+
+    if not alert or alert == "": return dashboard_basic_html
+    else: return '''<script type="text/javascript">alert("{}");</script>'''.format(alert) + dashboard_basic_html
+
+@app.route('/countries', methods=["GET"])
+@cross_origin()
+def get_countires():
+    return json.dumps({"countries": list(map(lambda x: x['name'], global_covid.list_countries()))})
+
+@app.route('/country/<c_name>', methods=["GET"])
+@cross_origin()
+def get_county_by_name(c_name):
+    return json.dumps( global_covid.country_stat(country_name=c_name, refresh=True) )
+
+def scheduled_job():
+    print("\nTimed Execution...")
+
 if __name__ == '__main__':
+
+    global_covid = CovidStats()
+
+    scheduler = BackgroundScheduler()
+    job = scheduler.add_job(scheduled_job, 'interval', minutes=1//60)
+    scheduler.start()
+
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0',port=port,threaded=True,debug=True)
